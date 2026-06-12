19b5a0b21b19389d80e72f65374a6949e27d75ed
Steven Green <stevengreen@Stevens-MacBook-Pro.local>
2026-06-11 23:06:06 -0700

    created style components

--- client/src/components/DeleteCocktail/index.jsx
+++ client/src/components/DeleteCocktail/index.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 
+import {
+  StyledDeleteSection,
+  StyledDeleteForm
+} from './style';
+
 function DeleteCocktail() {
   const [id, setId] = useState('');
   const [message, setMessage] = useState('');
@@ -27,36 +32,35 @@ function DeleteCocktail() {
   };
 
   return (
-    <div className="createContainer">
-        <div className="createdDrink">
-        <h2>Delete a Cocktail</h2>
+      <StyledDeleteSection> 
+        <div>
+          <h2>Delete a Cocktail</h2>
+            <StyledDeleteForm>
+              <form onSubmit={handleDelete}>
+                  <div>
+                      <input
+                      type="number"
+                      id="id"
+                      value={id}
+                      onChange={(e) => setId(e.target.value)}
+                      placeholder="Enter cocktail ID"
+                      required
+                      />
 
-        <div className="createContainerForm delete">
-            <form onSubmit={handleDelete}>
-            <div className="deleteIdRow">
-                <input
-                type="number"
-                id="id"
-                value={id}
-                onChange={(e) => setId(e.target.value)}
-                placeholder="Enter cocktail ID"
-                required
-                />
-
-                <button type="submit">
-                Delete Cocktail
-                </button>
-            </div>
-            </form>
-        </div>
+                      <button type="submit">
+                      Delete Cocktail
+                      </button>
+                  </div>
+              </form>
+            </StyledDeleteForm>
 
         {message && (
-            <div className="deleteMessage">
-            <p>{message}</p>
+            <div>
+                <p>{message}</p>
             </div>
         )}
         </div>
-    </div>
+      </StyledDeleteSection>
   );
 }
 
---
+++ client/src/components/DeleteCocktail/style.jsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const StyledDeleteSection = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  color: black;
+`;
+
+export const StyledDeleteForm = styled.div`
+  display: flex;
+  max-width: 600px;
+  margin: 30px;
+  padding: 20px;
+  border: 1px solid black;
+  border-radius: 5px;
+
+  div {
+    display: flex;
+    justify-content: space-around;
+    gap: 10px;
+    padding: 10px 0px;
+  }
+
+  input,
+  textarea {
+    padding: 10px;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+
+  button {
+    color: white;
+    background-color: red;
+    border-radius: 5px;
+  }
+  button:hover {
+    background-color: rgb(255, 89, 89);
+  }
+`;
