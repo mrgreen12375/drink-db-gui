868ce1e5c160309c4f73c2b7f9fca711e2174834
Steven Green <stevengreen@Stevens-MacBook-Pro.local>
2026-06-14 18:11:46 -0700

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,10 +9,18 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
-app.use(routes);
+// API ROUTES
+app.use("/api", routes);
 
-// turn on connection to db and server
+// SERVE REACT BUILD
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// SPA FALLBACK (CRITICAL)
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
+});
+
+// DB + SERVER START
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
