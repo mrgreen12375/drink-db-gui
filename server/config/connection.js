07359ea175ff195105f2c139aad7f1635ac7ab78
Steven Green <stevengreen@Stevens-MacBook-Pro.local>
2026-06-14 17:41:39 -0700

    updated connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,8 +2,16 @@ require("dotenv").config();
 
 const Sequelize = require("sequelize");
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
   : new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
